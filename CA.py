2394a6224d4f38f2a4aeb175bc714c583bf92206
wuthichat <69157523+Wuthichat@users.noreply.github.com>
2024-09-12 10:38:56 +0700

    Update CA.py

--- CA.py
+++ CA.py
@@ -1,21 +1,24 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
 SV = {
-   1: [1,5],
-   2: [2,6],
-   3: [3,7],
-   4: [4,8],
-   5: [0,8],
-   6: [1,9],
-   7: [0,7],
-   8: [1,8],
-   9: [2,9],
-  10: [1,2]
-  }
+   1: [1, 5],
+   2: [2, 6],
+   3: [3, 7],
+   4: [4, 8],
+   5: [0, 8],
+   6: [1, 9],
+   7: [0, 7],
+   8: [1, 8],
+   9: [2, 9],
+  10: [1, 2]
+}
 
 def generate_G1():
     OutputBit = []  
     G1 = [1] * 10   
-    for i in range(1023):
-        NewBits = G1[2] ^ G1[9]  # "^" --> XOR 
+    for i in range(100):
+        NewBits = G1[2] ^ G1[9]  # XOR operation
         G1.insert(0, NewBits)    
         OutputBit.append(G1.pop())  
     return OutputBit
@@ -23,11 +26,11 @@ def generate_G1():
 def generate_G2(tap):
     OutputBit = []
     G2 = [1]*10
-    for i in range(1023):
+    for i in range(100):
         NewBits = G2[1] ^ G2[2] ^ G2[5] ^ G2[7] ^ G2[8] ^ G2[9]
-        G2.insert(0,NewBits)
+        G2.insert(0, NewBits)
         G2.pop()
-        OutputBit.append(G2[SV[tap][0]]^G2[SV[tap][1]]) #PRN --> N
+        OutputBit.append(G2[SV[tap][0]] ^ G2[SV[tap][1]])  # PRN code generation
     return OutputBit
 
 def generate_GoldCode(tapA):
@@ -37,9 +40,18 @@ def generate_GoldCode(tapA):
     for i in range(len(G1)):
         CA.append(G1[i] ^ G2[i])
     return CA
+    
+def autocorrelate():
+  for i in SV.keys():
+      CA = generate_GoldCode(i)
+      for j in range(len(CA)):
+          CA[j] = (-1)**(1 + CA[j])
+      Correlate = np.correlate(CA , CA , mode = 'full')
+      plt.plot(Correlate)
+      plt.title(f'Autocorrelation Plot : {i}')
+      plt.xlabel('Lag')
+      plt.ylabel('Correlation')
+      plt.grid(True)
+      plt.show()
 
-for i in range(1,len(SV)+1):
-    print(f"generate_GoldCode:{i}",generate_GoldCode(i))
-
-
-#print(f"generate_GoldCode {6}",generate_GoldCode(6))
+autocorrelate()
